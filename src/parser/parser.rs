3011b25a616760360dd83663c3c25e754e445809
David Harwardt <david.harwardt@icloud.com>
2022-06-26 15:09:21 +0200

    rewrote ast

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,88 +1,64 @@
-use crate::tokenizer::{TokenType, Token, Keyword, Char, Brace, BracketType, Operator};
 
-pub enum Expression {
-    Add(Box<Expression>, Box<Expression>),
-    Sub(Box<Expression>, Box<Expression>),
-    Mult(Box<Expression>, Box<Expression>),
-    Div(Box<Expression>, Box<Expression>),
-    Value(i64),
-    SymbolValue(String),
+pub struct ProgramNode {
+    body: Vec<StatementNode>,
 }
 
-pub enum Instruction {
-    Named(u64, String),                     // optcode, name
-    Number(u64),                            // optcode
+pub enum StatementNode {
+    Include {
+        path: StringLiteralNode,
+    },
+
+    Define {
+        identifier: IdentifierNode,
+        expression: ExpressionNode,
+    },
+
+    MacroCreate {
+        identifier: IdentifierNode,
+        parameters: Vec<IdentifierNode>,
+        statements: Vec<StatementNode>,
+    },
+
+    LabelCreate {
+        identifier: IdentifierNode,
+    },
+
+    Instruction {
+        optcode: u64,
+        argument: ExpressionNode,
+    },
+
+    ValueDefinition {
+        value: ExpressionNode,
+    },
+
+    MacroCall {
+        identifier: IdentifierNode,
+        arguments: Vec<ExpressionNode>
+    },
 }
 
-pub enum Statement {
-    Label(String),                          // name of the label
-    Instruction(Instruction, Expression),   // instruction, argument
-    MacroCall(String, Vec<Expression>),     // name of macro, arguments for macro call
+pub struct IdentifierNode(String);
+
+pub enum ExpressionNode {
+    Binary {
+        a: Box<ExpressionNode>,
+        b: Box<ExpressionNode>,
+        operator: OperatorNode,
+    },
+
+    Literal(NumberLiteralNode),
+    Identifier(IdentifierNode),
 }
 
-pub enum AstNode {
-    Program(Vec<AstNode>),                  // children of program
+pub struct NumberLiteralNode(u64);
+pub struct StringLiteralNode(String);
 
-    Include(String),                        // include path
-    Define(String, Expression),             // define name, define value
-    MacroCreate(String, Vec<Statement>),    // name of macro, macro body
-
-    Statement(Statement),
-    Expression(Expression),
-
-    Collection(Vec<AstNode>)
-}
-
-pub enum ParserError {
-    Default(String),
-    UnbalancedBracketsError,
-}
-
-pub struct ParserResult {
-    length: usize,
-    node: AstNode,
-}
-
-pub trait Parser {
-    fn digest(&self, tokens: &[TokenType]) -> Result<ParserResult, ParserError>;
-
-    fn digest_all(&self, tokens: &[Token]) -> Result<AstNode, ParserError> {
-        todo!()
-    }
-}
-
-// match parser
-macro_rules! match_parser {
-    () => {
-        
-    };
-}
-
-// sequence parser
-pub struct SequenceParserItem {
-    pub parser: Box<dyn Parser>,
-    pub keep: bool,
-}
-pub struct SequenceParser {
-    pub parsers: Vec<SequenceParserItem>,
-}
-impl Parser for SequenceParser {
-    fn digest(&self, tokens: &[TokenType]) -> Result<ParserResult, ParserError> {
-        let mut idx = 0;
-        let mut res_vec = Vec::new();
-
-        for parser in &self.parsers {
-            let res = parser.parser.digest(&tokens[idx..])?;
-
-            if parser.keep { res_vec.push(res.node) }
-            idx += res.length;
-        }
-
-        Ok(ParserResult {
-            length: idx,
-            node: AstNode::Collection(res_vec)
-        })
-    }
+pub enum OperatorNode {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
 }
 
 // program
@@ -91,7 +67,7 @@ impl Parser for SequenceParser {
 //      define
 //          Percent, Keyword(Define), Symbol([name]), ExpressionParser,
 //      macro-create
-//          Keyword(Macro), Symbol([name]), Char(Bang), Until<Keyword(End), StatementParser>, Keyword(End)
+//          Keyword(Macro), Symbol([name]), Until<Keyword(End), StatementParser>, Keyword(End)
 //
 //      [StatementParser]
 //          label
